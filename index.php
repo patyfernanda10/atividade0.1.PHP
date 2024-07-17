55ffaa4ba69679433810951222fbfe0303e0272e
Paty <152908297+patyfernanda10@users.noreply.github.com>
2024-07-17 15:42:40 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+$valor1= 10;
+$valor2= 20; 
+$valor3= 30;
+$valor4= 5;
+$valor5= 6;  
+
+$soma = ($valor1 + $valor2 + $valor3 + $valor4 + $valor5) / 5;
+echo $soma
+?>
+
+<?php
+   $nota1= 1.0;
+   $nota2= 1.0; 
+   $nota3= 8.5;
+   $nota4= 9.0; 
+
+   $media = ($nota1 + $nota2 + $nota3 + $nota4) / 4;
+   echo $media;
+?>
+
+
+<?php
+    $nome ="patricia";
+    $valor1 = 10;
+    $valor2 = 12;
+
+    $divisao = ($valor1 / $valor2);
+    echo "$nome" . PHP_EOL  . "$divisao";
+    ?>
+
+<?php
+    $valor1 = 15;
+    $valor2 = 16;
+
+    $soma = ($valor1 + $valor2);
+    echo $soma;
+?>
